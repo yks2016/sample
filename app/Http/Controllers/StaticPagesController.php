e3d803daa24f9eb26c3ed445880a2b247db97471
vagrant <vagrant@homestead>
2017-06-07 14:33:16 +0000

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class StaticPagesController extends Controller
+{
+    public function home(){
+        return view('staticPages/home');
+    }
+
+    public function help(){
+        return view('staticPages/help');
+    }
+
+    public function about(){
+        return view('staticPages/about');
+    }
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title','Sample App') - Laravel入门教程</title>
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
