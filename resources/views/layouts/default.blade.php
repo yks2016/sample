d4b9af2925a043dbf3a3dd89166fe51715ed29ee
vagrant <vagrant@homestead>
2017-06-08 14:54:54 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,17 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
     <meta charset="utf-8">
     <title>@yield('title','Sample App') - Laravel入门教程</title>
-  </head>
-  <body>
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+  @include('layouts._header')
+<div class="container">
+  <div class="col-md-offset-1 col-md-10">
     @yield('content')
-  </body>
+    @include('layouts._footer')
+  </div>
+</div>
+</body>
 </html>
