c44a0bf536c1a03dd2b84e6752bc8dbc8b1f3e0f
vagrant <vagrant@homestead>
2017-06-13 14:40:05 +0000

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,12 +6,13 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-  @include('layouts._header')
+@include('layouts._header')
 <div class="container">
-  <div class="col-md-offset-1 col-md-10">
-    @yield('content')
-    @include('layouts._footer')
-  </div>
+    <div class="col-md-offset-1 col-md-10">
+        @include('shared.messages')
+        @yield('content')
+        @include('layouts._footer')
+    </div>
 </div>
 </body>
 </html>
