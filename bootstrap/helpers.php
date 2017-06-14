090858aff61971bae0b7b2e6e60c0a08d036424f
vagrant <vagrant@homestead>
2017-06-14 13:46:14 +0000

    Update database configuration

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,7 +2,7 @@
 function get_db_config()
 {
     if (getenv('IS_IN_HEROKU')) {
-        $url = parse_url(getenv('DADABASE_URL'));
+        $url = parse_url(getenv('DATABASE_URL'));
         return $db_config = [
             'connection' => 'pgsql',
             'host' => $url['host'],
